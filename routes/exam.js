d53e9774157c7f48ea708677578f476340f12c9e
ititandev <ldmanhit@gmail.com>
2019-10-27 17:10:22 +0700

    add GET POST PUT DELETE /api/subjects

--- routes/exam.js
+++ routes/exam.js
@@ -34,7 +34,7 @@ router.put("/classes/:id", (req, res) => {
         return fail(res, "Only admin can edit classes")
     ClassModel.updateOne({ _id: req.params.id }, { name: req.body.name }, (err, r) => {
         if (err) return error(res, err)
-        return success(res, "Edit " + r.nModified + " the class successfully")
+        return success(res, "Edit the class successfully")
     })
 })
 
@@ -47,5 +47,45 @@ router.delete("/classes/:id", (req, res) => {
     })
 })
 
+
+router.get("/subjects", (req, res) => {
+    SubjectModel.find((err, subjects) => {
+        if (err) return error(res, err)
+        return success(res, subjects)
+    })
+})
+
+router.post("/subjects", (req, res) => {
+    if (req.authz.role != "admin")
+        return fail(res, "Only admin can create subjects")
+    SubjectModel.find({ name: req.body.name }, (err, subjects) => {
+        if (err) return error(res, err)
+        if (subjects.length > 0)
+            return fail(res, "Subject existes")
+        SubjectModel.create({ name: req.body.name }, (err, c) => {
+            if (err) return error(res, err)
+            return success(res, c)
+        })
+    })
+
+})
+
+router.put("/subjects/:id", (req, res) => {
+    if (req.authz.role != "admin")
+        return fail(res, "Only admin can edit subjects")
+    SubjectModel.updateOne({ _id: req.params.id }, { name: req.body.name }, (err, r) => {
+        if (err) return error(res, err)
+        return success(res, "Edit the subject successfully")
+    })
+})
+
+router.delete("/subjects/:id", (req, res) => {
+    if (req.authz.role != "admin")
+        return fail(res, "Only admin can delete subjects")
+    SubjectModel.deleteOne({_id: req.params.id}, err => {
+        if (err) return error(res, err)
+        return success(res, "Delete the subject successfully")
+    })
+})
 
 module.exports = router;
