ac533d52e8f0d2cc4cc8ada248e312364b2172ac
ititandev <ldmanhit@gmail.com>
2019-11-07 01:46:11 +0700

    update static

---
+++ public/precache-manifest.18c792adca1ad354897a184f34336f0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb92f5faeb3965d4f9aef13f726fd2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20bd9899d3b7a3cd8684",
+    "url": "/static/css/main.e62d4b06.chunk.css"
+  },
+  {
+    "revision": "b07c1873b183f16a3f9b",
+    "url": "/static/js/2.8df04496.chunk.js"
+  },
+  {
+    "revision": "20bd9899d3b7a3cd8684",
+    "url": "/static/js/main.a0fdbfe9.chunk.js"
+  },
+  {
+    "revision": "98ca41096393d8622feb",
+    "url": "/static/js/runtime-main.dd99a021.js"
+  }
+]);
