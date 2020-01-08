08b87de0884783f2ea055de3a7ee14ae76711766
ititandev <ldmanhit@gmail.com>
2020-01-09 00:12:30 +0700

    fix LessonModel

--- schema/LessonModel.js
+++ schema/LessonModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const LessonSchema = new Schema({
     name: {type: String, required: true },
     datetime: { type: Date, default: Date.now },
-    contentId: { type: Schema.Types.ObjectId, ref: 'content', required: true }
+    contentId: { type: Schema.Types.ObjectId, ref: 'contents', required: true }
 });
 const LessonModel = mongoose.model('lessons', LessonSchema);
 
