eb651819533bf38c670d4e50404baaea44aafda6
ititandev <ldmanhit@gmail.com>
2020-03-29 00:28:46 +0700

    fixed number of ads when GET /ad

--- routes/ads.js
+++ routes/ads.js
@@ -6,44 +6,37 @@ var ObjectId = require("mongoose").Types.ObjectId;
 
 
 router.get("/ad", (req, res) => {
-    if (!req.query.horizontal)
-        req.query.horizontal = 1
-    if (!req.query.vertical)
-        req.query.vertical = 1
-
-    if (req.query.horizontal < 0)
-        return fail(res, "Không hợp lệ")
-    if (req.query.vertical < 0)
-        return fail(res, "Không hợp lệ")
-
 
     AdModel.find({ active: true, type: "horizontal" }, (err, horizontal) => {
         if (err) return error(res, err)
 
-        while (horizontal.length < req.query.horizontal) {
-            idx = Math.floor(Math.random() * horizontal.length);
-            addElement = horizontal[Math.floor(Math.random() * horizontal.length)];
-            horizontal.splice(idx, 0, addElement)
-        }
-        while (horizontal.length > req.query.num) {
-            idx = Math.floor(Math.random() * horizontal.length);
-            horizontal.splice(idx, 1);
+        if (horizontal.length > 0) {
+            while (horizontal.length < 1) {
+                idx = Math.floor(Math.random() * horizontal.length);
+                addElement = horizontal[Math.floor(Math.random() * horizontal.length)];
+                horizontal.splice(idx, 0, addElement)
+            }
+            while (horizontal.length > 1) {
+                idx = Math.floor(Math.random() * horizontal.length);
+                horizontal.splice(idx, 1);
+            }
         }
 
         AdModel.find({ active: true, type: "vertical" }, (err, vertical) => {
             if (err) return error(res, err)
-
-            while (vertical.length < req.query.vertical) {
-                idx = Math.floor(Math.random() * vertical.length);
-                addElement = vertical[Math.floor(Math.random() * vertical.length)];
-                vertical.splice(idx, 0, addElement)
+            if (vertical.length > 0) {
+                while (vertical.length < 2) {
+                    idx = Math.floor(Math.random() * vertical.length);
+                    addElement = vertical[Math.floor(Math.random() * vertical.length)];
+                    vertical.splice(idx, 0, addElement)
+                }
+                while (vertical.length > 2) {
+                    idx = Math.floor(Math.random() * vertical.length);
+                    vertical.splice(idx, 1);
+                }
             }
-            while (vertical.length > req.query.num) {
-                idx = Math.floor(Math.random() * vertical.length);
-                vertical.splice(idx, 1);
-            }
 
-            return success(res, {horizontal, vertical})
+            return success(res, { horizontal, vertical })
         })
     })
 })
